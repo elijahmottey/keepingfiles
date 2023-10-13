b3713418bd66012c8dbb8ec27f6a6cca2bfd4ba4
Elijah Mottey <elijahmottey5@gmail.com>
2023-10-13 01:22:29 +0000

    card

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Features from './components/Features';
 import Productivity from './components/productivity';
 import Team from './components/Team';
-
+import Image from './fylo-dark-theme-landing-page-master/images/bg-quotes.png'
 
 
 function App() {
@@ -14,7 +14,11 @@ function App() {
         <Header/>
         <LandingPage/>
         <Features/>
+       
         <Productivity/>
+        <figure className="quote">
+        <img src= {Image} alt="image" />
+        </figure>
         <Team/>
     </div>
   );
